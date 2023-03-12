abcd4ba177399a58941dd0468ea08c48724b1d1e
Normalniy-nick <126960965+Normalniy-nick@users.noreply.github.com>
2023-03-12 15:01:24 +0300

    Добавил комментарии к задаче поиска 3-го четвергов в году

--- free_day_in_museum.py
+++ free_day_in_museum.py
@@ -2,16 +2,15 @@ import calendar
 from datetime import datetime, date
 
 year = int(input())
-sp = []
 count = 0
 for month in range(1, 13):
-    for week in calendar.monthcalendar(year, month):
-        if week[3] < 1:
+    for week in calendar.monthcalendar(year, month): # список дней по неделям
+        if week[3] < 1:  # проверка первой недели на присутствие четверга, если четверга нет, то будет нулевое значение
             continue
         count += 1
-        if count == 3:
+        if count == 3:  # каждая третья неделя, где есть четверг
             tuesday = week[3]
-            count = 0
+            count = 0  # обнуляем счётчик четверга
             if tuesday:
                 data = date(year, month, tuesday)
                 print(datetime.strftime(data, '%d.%m.%Y'))
